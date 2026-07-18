4fc78a9598c13acc825bef97afdf921ee3ca60f7
Prakash Jangid <prakashjangid7357@gmail.com>
2026-07-18 11:14:24 +0530

    feat: remove form field service and related models, clean up unused code

--- packages/trpc/server/services/index.ts
+++ packages/trpc/server/services/index.ts
@@ -1,5 +1,4 @@
 export  * as userService from "@repo/services/user"
 export * as formService from "@repo/services/form"
-export * as formFieldService from "@repo/services/form-field"
 export * as responseService from "@repo/services/responses"
 export * as analyticsService from "@repo/services/analytics"
